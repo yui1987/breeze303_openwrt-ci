9cfb481b92a1d31580bc81471c0941dd02c6911f
yui1987 <140272762+yui1987@users.noreply.github.com>
2024-07-28 08:53:28 +0800

    Update diy-script-1.sh

--- diy-script-1.sh
+++ diy-script-1.sh
@@ -89,8 +89,7 @@ sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 
-# x86 型号只显示 CPU 型号
-sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
+
 
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
