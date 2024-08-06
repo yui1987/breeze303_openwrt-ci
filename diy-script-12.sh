a239c726a0508b279034e154cec96580c0c0d10e
yui1987 <140272762+yui1987@users.noreply.github.com>
2024-08-07 00:06:13 +0800

    Update diy-script-12.sh

--- diy-script-12.sh
+++ diy-script-12.sh
@@ -15,56 +15,60 @@ function git_sparse_clone() {
 
 
 
-# 稀疏克隆软件     muluxia   packages  xia
+# 稀疏克隆软件     目录为主目录下   packages
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adbyby-plus
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages adbyby
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-wrtbwmon
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages wrtbwmon
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-disks-info
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+# git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-netspeedtest
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages homebox
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-speedtest-web
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages speedtest-web
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-unblockneteasemusic
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-opkg
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages opkg
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-alist
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages alist
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-cpulimit
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages cpulimit
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-ddns-go
 #git_sparse_clone master https://github.com/kiddin9/openwrt-packages ddns-go
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-disks-info
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-netspeedtest
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages homebox
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-speedtest-web
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages speedtest-web
-#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-unblockneteasemusic
 
-# git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-onliner
+# 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+# git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-onliner
 
+# 科学
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-bypass
-# git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-v2raya
 # git_sparse_clone master https://github.com/kiddin9/openwrt-packages v2raya
+# git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 # passwall和易有云文件管理在feed里
 
-# git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-# git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 
+
+# 主题
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
+# git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 可能用到的外部支持  
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-shadowsocks-libev
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-shadowsocks-rust
-git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocks-rust
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-shadowsocks-libev
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-shadowsocks-rust
+#git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocks-rust
 
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/emortal/autocore/files/*/index.htm
@@ -74,7 +78,7 @@ date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/emortal/default-settings/files/99-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by Haiibo/g" package/emortal/default-settings/files/99-default-settings
 
-#ssrplus和bypass有有一个依赖错误 shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-server
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
